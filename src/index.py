c01a40d881a035362e1086d49f06fee8fa230724
Vladimir0657305 <vv0657305@gmail.com>
2023-05-09 18:40:31 +0300

    changes

--- src/index.py
+++ src/index.py
@@ -1,10 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
 import os
 import dotenv
@@ -12,15 +14,17 @@ import requests
 import time
 from selenium.common.exceptions import TimeoutException
 
-
+# Загрузка настроек из файла .env
 from dotenv import load_dotenv
 load_dotenv()
 
-# proxy_username = os.getenv('PROXY_USERNAME')
-# proxy_password = os.getenv('PROXY_PASSWORD')
-proxy_username = 'brd-customer-hl_db7162d7-zone-unblocker'
-proxy_password = '2ksf5kpyn6pk'
+# Получение учетных данных прокси-сервера из переменных окружения
+proxy_username = os.getenv('PROXY_USERNAME')
+proxy_password = os.getenv('PROXY_PASSWORD')
+# proxy_username = 'lum-customer-CUST_ID-zone-ZONE_NAME'
+# proxy_password = 'PASSWORD'
 
+# Указание настроек прокси-сервера
 PROXY_HOST = "zproxy.lum-superproxy.io"
 PROXY_PORT = "22225"
 
@@ -29,49 +33,39 @@ proxy = Proxy({
     'httpProxy': f"{proxy_username}:{proxy_password}@{PROXY_HOST}:{PROXY_PORT}"
 })
 
+# Опции браузера Chrome
 chrome_options = Options()
 chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument("--proxy-server=http://%s" % proxy.no_proxy)
 
-driver = webdriver.Chrome(service=Service(executable_path="C:/Program Files/Pyton/chromedriver"), options=chrome_options)
+# Указание пути к исполняемому файлу драйвера Chrome
+driver_path = "C:/Program Files/Pyton/chromedriver"
 
-# def get_doctors_links(page):
-#     url = f'https://www.idf.co.uk/patients/find-a-doctor.aspx?Specialty=20&SubSpecialty=0&AreaCode=W1G&SearchCriteria=London&PageNumber={page}'
-#     driver.get(url)
-#     time.sleep(55) 
-#     wait = WebDriverWait(driver, 10)
-#     wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.results-summary')))
-#     html = driver.page_source
-#     soup = BeautifulSoup(html, 'html.parser')
-#     links = soup.select('.docresults a[href]')
-#     doctor_links = [link['href'] for link in links]
-#     return doctor_links
+# Инициализация драйвера Chrome
+driver = webdriver.Chrome(service=Service(executable_path=driver_path), options=chrome_options)
 
-def get_doctors_links(page):
-    url = f'https://www.idf.co.uk/patients/find-a-doctor.aspx?Specialty=20&SubSpecialty=0&AreaCode=W1G&SearchCriteria=London&PageNumber={page}'
-    wait = WebDriverWait(driver, 20)
-    while True:
-        try:
-            driver.get(url)
-            wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.results-summary')))
-            break
-        except TimeoutException:
-            driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    links = soup.select('.docresults a[href]')
-    doctor_links = [link['href'] for link in links]
-    return doctor_links
+# Выполнение запроса с использованием прокси-сервера
+url = f'https://www.idf.co.uk/patients/find-a-doctor.aspx?Specialty=20&SubSpecialty=0&AreaCode=W1G&SearchCriteria=London&PageNumber=1'
+driver.get(url)
 
+# Ожидание загрузки страницы
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.section')))
 
+# Ввод текста в поисковую строку и отправка запроса
+# search_box = driver.find_element(By.CSS_SELECTOR, 'input#id-search-field')
+# search_box.send_keys('beautiful soup')
+# search_box.submit()
 
-def main():
-    print(proxy_username, proxy_password)
-    
-    doctor_links = get_doctors_links(1)
-    print(driver.current_url)
-    print(doctor_links)
+# Ожидание загрузки страницы с результатами поиска
+# wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.list-recent-events')))
 
+# Получение HTML-кода страницы с результатами поиска
+print(proxy_username, proxy_password)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+links = soup.select('.docresults a[href]')
+doctor_links = [link['href'] for link in links]
 
-if __name__ == '__main__':
-    main()
+# Закрытие браузера
+driver.quit()
