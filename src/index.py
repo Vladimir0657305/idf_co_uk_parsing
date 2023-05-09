0c2935dd4329b8fe7b0b93949695d8def26cfd98
Vladimir0657305 <vv0657305@gmail.com>
2023-05-09 22:45:36 +0300

    changes

--- src/index.py
+++ src/index.py
@@ -8,12 +8,15 @@ from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+import re
 import os
 import dotenv
 import requests
 import time
 from selenium.common.exceptions import TimeoutException
 from urllib.parse import urljoin
+import csv
 
 # Загрузка настроек из файла .env
 from dotenv import load_dotenv
@@ -27,12 +30,11 @@ response = requests.get(url_page)
 soup_page = BeautifulSoup(response.content, 'html.parser')
 pages = soup_page.find('p', class_='center').find_all('a')
 last_page = int(pages[-1].get_text(strip=True))
+last_page = 2
 
 # Получение учетных данных прокси-сервера из переменных окружения
 PROXY_USERNAME = os.getenv('PROXY_USERNAME')
 PROXY_PASSWORD = os.getenv('PROXY_PASSWORD')
-# proxy_username = 'lum-customer-CUST_ID-zone-ZONE_NAME'
-# proxy_password = 'PASSWORD'
 
 # Указание настроек прокси-сервера
 PROXY_HOST = "zproxy.lum-superproxy.io"
@@ -54,36 +56,105 @@ driver_path = "C:/Program Files/Pyton/chromedriver"
 # Инициализация драйвера Chrome
 driver = webdriver.Chrome(service=Service(executable_path=driver_path), options=chrome_options)
 
-# Выполнение запроса с использованием прокси-сервера
-url = f'https://www.idf.co.uk/patients/find-a-doctor.aspx?Specialty=20&SubSpecialty=0&AreaCode=W1G&SearchCriteria=London&PageNumber=1'
-driver.get(url)
+# Создаем список для хранения всех ссылок на врачей
+all_doctor_links = []
 
-# Ожидание загрузки страницы
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.section')))
+# Создаем цикл для перебора всех страниц
+for page in range(1, last_page+1):
+    # Формируем ссылку на текущую страницу
+    url = f'https://www.idf.co.uk/patients/find-a-doctor.aspx?Specialty=20&SubSpecialty=0&AreaCode=W1G&SearchCriteria=London&PageNumber={page}'
 
-# Ввод текста в поисковую строку и отправка запроса
-# search_box = driver.find_element(By.CSS_SELECTOR, 'input#id-search-field')
-# search_box.send_keys('beautiful soup')
-# search_box.submit()
+    # Загружаем страницу
+    driver.get(url)
 
-# Ожидание загрузки страницы с результатами поиска
-# wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.list-recent-events')))
+    # Ожидание загрузки страницы
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.section')))
 
-# Получение HTML-кода страницы с результатами поиска
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.select('.docresults a[href]')
-doctor_links = [urljoin(base_url, link['href']) for link in links]
-print(doctor_links)
+    # Получение HTML-кода страницы с результатами поиска
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.select('.docresults a[href]')
+    doctor_links = [urljoin(base_url, link['href']) for link in links]
 
-# for href in hrefs:
-#     full_url = urljoin(base_url, href)
-#     print(full_url)
+    # Добавляем найденные ссылки на врачей на текущей странице в общий список
+    all_doctor_links.extend(doctor_links)
 
-# for link in links:
-#     print(link['href'])
+# Открываем CSV-файл для записи
+with open('doctors.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+
+    # Записываем заголовки столбцов
+    writer.writerow(['Counter', 'Name', 'Qualifications', 'Specialty', 'Address', 'Telephone', 'Email', 'Website', 'Biography', 'Research Interests'])
+
+    # Создаем глобальный счетчик
+    counter = 1
+
+    # Создаем цикл для перебора всех врачей
+    for doctor_link in all_doctor_links:
+        # Загружаем страницу врача
+        driver.get(doctor_link)
+        # Ожидание загрузки страницы
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.content3')))
+
+    # Получение HTML-кода страницы врача
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Извлечение данных врача
+    try:
+        name = soup.select_one('h2.memberprofile').get_text(strip=True)
+    except AttributeError:
+        name = 'No name data'
+    try:
+        qualifications = soup.select_one('.qualifications').get_text(strip=True)
+    except AttributeError:
+        qualifications = 'No qualifications data'
+    try:
+        specialty_label = soup.select_one('.profile-specialty')
+        specialty = specialty_label.contents[1].strip()
+    except AttributeError:
+        specialty = 'No specialty data'
+    try:
+        address_label = soup.find('li', {'id': 'ctl00_MainContentPlaceHolder_AddressLi'})
+        address_items = address_label.find_all()
+        address = ''.join([str(item).strip() for item in address_items if not (item.name == 'span' and item.get('class') == 'strong')])
+        address = re.sub('<[^<]+?>', '', address)
+        address = address.replace('\n', ' ')
+    except AttributeError:
+        address = 'No address data'
+    try:
+        telephone_label = soup.find('li', {'id': 'ctl00_MainContentPlaceHolder_TelephoneLi'})
+        telephone_span = telephone_label.find('span', {'class': 'strong'}, text='Appointments Telephone:')
+        telephone = telephone_span.find_next_sibling('br').next_sibling.strip().replace('Tel: ', '')
+    except AttributeError:
+        telephone = 'No telephone data'
+    try:
+        email_label = soup.find('li', {'id': 'ctl00_MainContentPlaceHolder_EmailAddressLi'})
+        email = email_label.find('a', href=True).text.strip()
+    except AttributeError:
+        email = 'No email data'
+    try:
+        website_label = soup.find('li', {'id': 'ctl00_MainContentPlaceHolder_WebsiteLi'})
+        website = website_label.find('a', href=True).text.strip()
+    except AttributeError:
+        website = 'No website data'
+    
+
+    print(f"Name: {name}")
+    print(f"Qualifications: {qualifications}")
+    print(f"Specialty: {specialty}")
+    print(f"Address: {address}")
+    print(f"Telephone: {telephone}")
+    print(f"Email: {email}")
+    print(f"Website: {website}")
 
 
-# Закрытие браузера
+    # Записываем данные врача в CSV-файл
+    writer.writerow([counter, name, qualifications, specialty, address, telephone, email, website])
+
+    # Увеличиваем глобальный счетчик на 1
+    counter += 1
+# Закрываем браузер
 driver.quit()
